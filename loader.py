cfa139baad75ffead8254c5acb6f8e2612788cc1
Punit Arani <punitsai36@gmail.com>
2023-06-01 07:53:48 -0700

    Add chunk_text()

--- loader.py
+++ loader.py
@@ -1,5 +1,7 @@
 """loader.py"""
 
+import re
+
 import fitz
 
 from utils import format_text
@@ -8,7 +10,7 @@ from utils import format_text
 def read_pdf(data: bytes) -> str:
     """
     Read PDF data and return text
-    :param data: PDF data as bytes
+    :param data: PDF data as bytes string
     :return: Text string from PDF
     """
 
@@ -17,3 +19,51 @@ def read_pdf(data: bytes) -> str:
         text = b"\n".join(text_pages).decode("utf-8")
     text = format_text(text)
     return text
+
+
+def chunk_text(text: str, min_chars: int = 40, max_chars: int = 160) -> list[str]:
+    """
+    Chunk text into smaller chunks
+    :param text: The text to chunk
+    :param min_chars: The minimum number of characters per chunk
+    :param max_chars: The maximum number of characters per chunk
+    :return: A list of text chunks
+    """
+    # Regular expression pattern to split text into sentences
+    sentence_pattern = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
+
+    # Splitting the text into sentences using the pattern
+    sentences = re.split(sentence_pattern, text)
+
+    # List to store the chunks of sentences
+    chunks = []
+
+    def split_sentences(sentence_list, current_chunk):
+        """
+        Split sentences into chunks recursively
+        :param sentence_list: List of sentences
+        :param current_chunk: The current chunk of sentences
+        """
+        if len(current_chunk) > max_chars:
+            # If the current chunk exceeds the max_chars limit, split it recursively
+            chunks.append(current_chunk[:max_chars].strip())
+            remaining_text = current_chunk[max_chars:]
+            split_sentences(sentence_list, remaining_text)
+        elif len(sentence_list) > 0:
+            # If there are remaining sentences, add them to the current chunk
+            current_sentence = sentence_list.pop(0)
+            if len(current_chunk) + len(current_sentence) <= max_chars:
+                current_chunk += current_sentence
+            else:
+                # If the current chunk reaches the max_chars limit, add it to the chunks list
+                if len(current_chunk) >= min_chars:
+                    chunks.append(current_chunk.strip())
+                current_chunk = current_sentence
+            split_sentences(sentence_list, current_chunk)
+        else:
+            # Add the last chunk to the list
+            if len(current_chunk) >= min_chars:
+                chunks.append(current_chunk.strip())
+
+    split_sentences(sentences, "")
+    return chunks
